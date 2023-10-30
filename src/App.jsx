79d7d256f87fdb5b06d9642513a71a6ee70358a3
Anésio Neto <anesios98@gmail.com>
2023-10-29 23:41:36 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="h-[812px] width-[375px] bg-white">
+      <div>
+        <img src="" alt="Woman with a flower" />
+      </div>
+      <div className="min-h-full"></div>
+      <h1 className="flex justify-center">Hello World!</h1>
+    </div>
+  );
+}
