c5b2793cd10927cb46291911a741c040056709cd
Anésio Neto <anesios98@gmail.com>
2023-10-30 00:12:29 -0300

    feat: added album header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import buttonImg from "./img/Button.png";
+import img from "./img/Image.png";
+
 export default function App() {
   return (
-    <div className="h-[812px] width-[375px] bg-white">
-      <div>
-        <img src="" alt="Woman with a flower" />
+    <div className="flex flex-col h-[812px] w-[375px] bg-white">
+      <div className="w-full">
+        <img src={img} alt="Woman with a flower" />
       </div>
-      <div className="min-h-full"></div>
-      <h1 className="flex justify-center">Hello World!</h1>
+
+      <section
+        className="flex justify-between items-center ml-7 pr-5 border-b-solid border-b-2 border-b-gray-100
+      "
+      >
+        <h1 className="text-3xl pt-3 pb-2 font-bold">FKA Twigs</h1>
+        <a>
+          <img src={buttonImg} alt="Album Options Button" />
+        </a>
+      </section>
     </div>
   );
 }
